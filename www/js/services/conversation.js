e6765e0cb56cf17e1cf262b7b77640d3e4b96c74
elisa <elisa15.07@free.fr>
2016-11-22 22:11:32 +0100

    messagerie : message + conversation + ajout message à un nouveau contact

---
+++ www/js/services/conversation.js
@@ -0,0 +1,32 @@
+﻿angular.module('starter').factory('Conversation', function (FURL, $log, $firebaseAuth, $firebaseArray, $firebaseObject, Utils) {
+
+    //var ref = new Firebase(FURL);
+
+  //  firebase.initializeApp(FURL);
+    //var auth = $firebaseAuth(ref);
+    var ref = firebase.database().ref();
+    console.log(firebase.database());
+    var Conversation = {
+        conv: {},
+
+        login: function (user) {
+            return auth.$signInWithEmailAndPassword(
+              user.email, user.password
+            );
+        },
+        creerConversation: function (uid, message,uidDest) {
+            var conv = {
+                id: uid,
+                idDest: uidDest,
+                message: message,
+                date: Date()
+            };
+
+            var messagesRef = $firebaseArray(firebase.database().ref().child("conversations"));
+            messagesRef.$add(conv);
+            $log.log("Conversation Saved");
+        },
+    };
+    return Conversation;
+
+});
--- www/templates/messagerie/messagerie.js
+++ www/templates/messagerie/messagerie.js
@@ -1,7 +1,28 @@
 ﻿'Use Strict';
 angular.module('starter').controller('messagerieController', function ($scope, $state, $localStorage, $location, $http, $ionicPopup, $firebaseAuth, $firebaseObject, $log, Auth, FURL, Utils, $ionicModal, Conversation) {
     $scope.modal = {};
+    $scope.user = { firstname: "Elisa", lastname: "Laurent", nomComplet:"Elisa Laurent", imageProfil:"img/bonhomme.jpg" };
+    $scope.voirConv = {};
     $scope.message = {};
+    $scope.conversation = {};
+    
+    $scope.messagerie = [
+        {
+            user: "Jean Martin",imageProfil:"img/homme.jpg", messages:
+                [
+              { message: "Alors tu fais quoi de beau maintenant ?", date: "22 novembre", heure: "11h43", auteur: $scope.user.firstname + " " + $scope.user.lastname },
+              { message: "Salut !", date: "22 novembre", heure: "11h43", auteur: $scope.user.firstname + " " + $scope.user.lastname },
+              { message: "Hey !" , date: "22 novembre", heure: "11h43", auteur: "Jean Martin" }
+            ]
+        },
+        {user: "Maria Jambon",imageProfil:"img/femme.jpg", messages:
+            [
+          { message: "Bonjour, Oui. Le poste est ... .. .... .... ..... .... ", date: "22 novembre", heure: "11h43", auteur: "Maria Jambon" },
+          { message: "Bonjour, votre offre pourrait m'intéressé, puis je en savoir plus sur le poste ?  !", date: "22 novembre", heure: "15h21", auteur: $scope.user.firstname + " " + $scope.user.lastname },
+          { message: "Bonjour, je suis interessé par votre profil, je cherche un dévelopeur web senior pour mon entreprise XXX basé à Grenoble. Êtes vous interessé ? Cordialement ", date: "22 novembre", heure: "13h43", auteur: "Maria Jambon" }
+            ]
+}
+    ];
     $ionicModal.fromTemplateUrl('templates/messagerie/newConversation.html', {
         scope: $scope,
         animation: 'slide-in-up'
@@ -11,17 +32,23 @@ angular.module('starter').controller('messagerieController', function ($scope, $
     $scope.newConversation = function () {
         $scope.modal.show();
     };
-   
-   
+    $ionicModal.fromTemplateUrl('templates/messagerie/conversation.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.voirConv = modal;
+    });
     $scope.modal.hide = function () {
         $scope.modal.hide();
     };
     $scope.nouveauMessage = function (message) {
         console.log(message);
-        var ref = firebase.database().ref();
-        var obj = $firebaseObject(ref);
-        console.log(obj);
+        $scope.messagerie.push({ user: message.user, imageProfil:"img/bonhomme.jpg",messages: [{ message: message.texte, date: new Date(), heure:new Date()}]});
         $scope.modal.hide();
-       Conversation.creerConversation(Auth.user,message.texte,message.user);
+    };
+    $scope.voirConversation = function (m) {
+        $scope.conversation = m;
+        console.log(m);
+        $scope.voirConv.show();
     };
 });
